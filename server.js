7ab8a1f22388426eb7eeb3bd60da601ffd12cb8b
mark7389 <mark.soliman73@gmail.com>
2017-12-25 15:15:20 -0500

    heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(express.static("client/build"));
+app.use(express.static("./client/build"));
 
 app.use(routes);
 
